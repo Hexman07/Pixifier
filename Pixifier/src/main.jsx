295986fccc01c21af5bb221f8fdf4009bf71f3e7
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-10-31 23:58:47 +0530

    [Fix]: Follow React@18 convention for reactDom

--- Pixifier/src/main.jsx
+++ Pixifier/src/main.jsx
@@ -1,13 +1,15 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import App from "./App";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
