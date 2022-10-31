bb811e92ca5d333c71d4da6a26d84628ea090575
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-10-31 23:46:56 +0530

    [Feat]: Improve page loads using suspence and lazy loading

--- Pixifier/src/App.jsx
+++ Pixifier/src/App.jsx
@@ -1,16 +1,16 @@
-import React, { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import CreatePost from "./pages/CreatePost";
-import { useSelector } from "react-redux";
-import toast from "react-hot-toast";
-import IndiPost from "./pages/IndiPost";
-import LikedPosts from "./pages/LikedPosts";
-import MyPosts from "./pages/MyPosts";
-import Account from "./pages/Account";
-import ProtectedRoute from "./pages/ProtectedRoute";
+import React, { useEffect, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Header from './components/Header';
+const Home = React.lazy(() => import('./pages/Home'));
+const Login = React.lazy(() => import('./pages/Login'));
+const CreatePost = React.lazy(() => import('./pages/CreatePost'));
+const toast = React.lazy(() => import('react-hot-toast'));
+const IndiPost = React.lazy(() => import('./pages/IndiPost'));
+const LikedPosts = React.lazy(() => import('./pages/LikedPosts'));
+const MyPosts = React.lazy(() => import('./pages/MyPosts'));
+const Account = React.lazy(() => import('./pages/Account'));
+const ProtectedRoute = React.lazy(() => import('./pages/ProtectedRoute'));
 
 const App = () => {
   const { isLoggedIn, isError, message } = useSelector((store) => store.pixify);
@@ -28,59 +28,61 @@ const App = () => {
   }, [isError, message]);
 
   return (
-    <div className="bg-gradient-to-br min-h-screen from-blue-900 via-sky-900 to-blue-900 bg-no-repeat">
+    <div className='bg-gradient-to-br min-h-screen from-blue-900 via-sky-900 to-blue-900 bg-no-repeat'>
       {isLoggedIn && <Header />}
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/createpost"
-          element={
-            <ProtectedRoute>
-              <CreatePost />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/post/:id"
-          element={
-            <ProtectedRoute>
-              <IndiPost />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/likedposts"
-          element={
-            <ProtectedRoute>
-              <LikedPosts />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/myposts"
-          element={
-            <ProtectedRoute>
-              <MyPosts />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/account"
-          element={
-            <ProtectedRoute>
-              <Account />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route
+            path='/'
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/createpost'
+            element={
+              <ProtectedRoute>
+                <CreatePost />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/post/:id'
+            element={
+              <ProtectedRoute>
+                <IndiPost />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/likedposts'
+            element={
+              <ProtectedRoute>
+                <LikedPosts />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/myposts'
+            element={
+              <ProtectedRoute>
+                <MyPosts />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/account'
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
